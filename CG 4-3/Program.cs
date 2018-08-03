537eed44ea7a3d05c7f3e2b93e17630d8eb38f89
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-03 14:49:04 -0500

    trying to work out the random picked name

--- CG 4-3/Program.cs
+++ CG 4-3/Program.cs
@@ -7,30 +7,43 @@ namespace CG_4_3
         static void Main(string[] args)
         {
 
-            Random winner = new Random();
+            //Random winner = new Random();
 
             //string[] array = new string[30];
-            Console.WriteLine("Please enter a name.");
-            string answer = Console.ReadLine();
+            //Console.WriteLine("Please enter a name.");
+            //string answer = Console.ReadLine();
+
+
             string[] name = new string[30];
 
             {
+                Console.WriteLine("Please enter a name.");
+                string answer = Console.ReadLine();
+
                 for (int names = 0; names < 31; names++)
                 {
+                    //Console.WriteLine("Please enter a name.");
+                    //string answer = Console.ReadLine();
+
                     string Names = Convert.ToString(names);
                     //if (names == null) break;
 
-                    if (string.IsNullOrEmpty(Names))
+
+                    if (string.IsNullOrEmpty(answer))
                     {
                         break;
                     }
+                }
 
-                int mIndex = winner.Next(name.Length);
-                Console.WriteLine("The winner of the raffle is {0}!!");
+                Random winner = new Random();
+                string Winner = answer[winner.Next(answer.Length)];
+                Console.WriteLine($"The winner of the raffle is {answer}!!");
+
+                    //int mIndex = winner.Next(name.Length);
+                    //Console.WriteLine($"The winner of the raffle is {mIndex}!!");
 
 
-            }
-
+             }
 
             Console.ReadLine();
         }
