4d64d2d564d0daff96eca43b2348a176bb084dee
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-02 23:04:08 -0500

    Beginning of initial code

--- CG 4-3/Program.cs
+++ CG 4-3/Program.cs
@@ -6,7 +6,23 @@ namespace CG_4_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            for (int Names = 0; Names < 31; Names++)
+            {
+                Console.WriteLine("Please enter a name.");
+                var name = Console.ReadLine();
+
+                if (name.Equals(null))
+                    {
+
+                }
+
+
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
