86171c84b3f2f6b15f9370dd3d78be2563a89f72
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-03 14:09:41 -0500

    Try to make it work again - messed with array

--- CG 4-3/Program.cs
+++ CG 4-3/Program.cs
@@ -12,18 +12,18 @@ namespace CG_4_3
             //string[] array = new string[30];
             Console.WriteLine("Please enter a name.");
             string answer = Console.ReadLine();
-            string[] name = new string[int.Parse(answer)];
+            string[] name = new string[(int.Parse(answer)];
 
             {
                 for (int names = 0; names < 31; names++)
                 {
+                    string Names = Convert.ToString(names);
+                    //if (names == null) break;
 
-                    if (names == null) break;
-                
-                //if (string.IsNullOrEmpty(names))
-                //    { 
-                //    break;
-                }
+                    if (string.IsNullOrEmpty(Names))
+                    {
+                        break;
+                    }
 
                 int mIndex = winner.Next(name.Length);
                 Console.WriteLine("The winner of the raffle is {0}!!");
