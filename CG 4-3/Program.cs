43734107396b0ef4e6c69a9178d8ecca41d6efd0
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-03 18:57:53 -0500

    Got myself turned around

--- CG 4-3/Program.cs
+++ CG 4-3/Program.cs
@@ -13,31 +13,38 @@ namespace CG_4_3
             //Console.WriteLine("Please enter a name.");
             //string answer = Console.ReadLine();
 
+            //string[] name = new string[(int.Parse(answer))];
+
 
             string[] name = new string[30];
 
             {
-                Console.WriteLine("Please enter a name.");
-                string answer = Console.ReadLine();
+                //Console.WriteLine("Please enter a name.");
+                //string answer = Console.ReadLine();
 
-                for (int names = 0; names < 31; names++)
+                for (int Names = 0; Names < 31; Names++)
+
+
                 {
-                    //Console.WriteLine("Please enter a name.");
-                    //string answer = Console.ReadLine();
+                    //This allows to have the question asked to enter the names
+                    Console.WriteLine("Please enter a name.");
+                    string answer = Console.ReadLine();
 
-                    string Names = Convert.ToString(names);
+                   string entered = Convert.ToString(Names);
+
+
                     //if (names == null) break;
 
-
-                    if (string.IsNullOrEmpty(answer))
-                    {
-                        break;
-                    }
+                
+                if (string.IsNullOrEmpty(entered))
+                {
+                    break;
+                }
                 }
 
-                Random winner = new Random();
-                string Winner = answer[winner.Next(answer.Length)];
-                Console.WriteLine($"The winner of the raffle is {answer}!!");
+                //Random winner = new Random();
+                //string Winner = answer[winner.Next(answer.Length)];
+                //Console.WriteLine($"The winner of the raffle is {answer}!!");
 
                     //int mIndex = winner.Next(name.Length);
                     //Console.WriteLine($"The winner of the raffle is {mIndex}!!");
